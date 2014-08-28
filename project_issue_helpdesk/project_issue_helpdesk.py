1a59fbbb3e2047de39998c6fe2a8d47d154cc966
Carlos Navarro <carlos.navarro@imesasistemas.com>
2014-08-28 08:53:37 -0600

    [Add]Add class contract.pricelist,contract.pricelist.line,holiday.calendar

--- project_issue_helpdesk/project_issue_helpdesk.py
+++ project_issue_helpdesk/project_issue_helpdesk.py
@@ -34,7 +34,7 @@ class ProjectIssue(osv.Model):
                 'warranty': fields.selection([('seller','Seller'),('manufacturer','Manufacturer')],string="Warranty"),                                 
                 'backorder_ids': fields.one2many('stock.picking.out','issue_id'),
                 'origin_id':fields.many2one('project.issue.origin',string="Origin"),
-                'partner_type':fields.related('partner_id','partner_type',relation='res.partner',string="Parter Type"),
+                'partner_type':fields.related('partner_id','partner_type',relation='res.partner',string="Partner Type"),
                 'categ_id':fields.many2one('product.category',required=True,string="Category Product"),
                 'product_id':fields.many2one('product.product',string="Product"),
                 'prodlot_id':fields.many2one('stock.production.lot',string="Serial Number"),
@@ -127,9 +127,12 @@ class ResPartner(orm.Model):
     _inherit = 'res.partner'
 
     _columns = {
-        'partner_type': fields.selection([('company','Company'),('branch','Branch'),('customer','Customer')],required=True,string="Partner Type")
-
-    }
+        'partner_type': fields.selection([('company','Company'),('branch','Branch'),('customer','Customer')],required=True,string="Partner Type"),
+        'provision_amount':fields.float(digits=(16,2),string="Provision Amount")
+     }
+    _defaults={
+        'provision_amount':0.0
+        }
     
     def onchange_partner_type(self, cr, uid, ids,partner_type,context={}):
         res={}
@@ -142,3 +145,75 @@ class ResPartner(orm.Model):
             res['is_company'] = False
         return {'value': res}
 
+class ContractPricelist(orm.Model):
+    _name = 'contract.pricelist'
+
+    _columns = {
+        'name':fields.char(size=256,string="Name"),
+        'account_analytic_id':fields.many2one('account.analytic.account',string="Account Analytic"),
+        'partner_id':fields.related('account_analytic_id','partner_id',relation='res.partner',string="Partner"),
+        'calendar_id':fields.many2one('resource.calendar',string="Calendar"),
+        'line_ids': fields.one2many('contract.pricelist.line,','contract_pricelist_id')
+        }
+    _sql_constraints = [
+        ('account_analytic_unique',
+        'UNIQUE(account_analytic_id)',
+        'Account Analytic already exist ')
+                        ]
+    
+class ContractPriceLine(orm.Model):
+    _name = 'contract.pricelist.line'
+
+    _columns = {
+        'contract_pricelist_id':fields.many2one('contract.pricelist',string="Contract Pricelist"),
+        'pricelist_line_type': fields.selection([('category','Category'),('product','Product')],string="Pricelist Type"),                                 
+        'categ_id':fields.many2one('product.category',string="Product Category"),
+        'product_id':fields.many2one('product.product',string="Product"),
+        'technical_rate':fields.float(digits=(16,2),required=True,string="Technical Rate"),
+        'assistant_rate':fields.float(digits=(16,2),required=True,string="Assistant Rate"),
+        'overtime_multiplier':fields.float(digits=(16,2),required=True,string="Overtime Multiplier"),
+        'holiday_multiplier':fields.float(digits=(16,2),required=True,string="Holiday Multiplier")
+       }
+    
+    _defaults={
+        'technical_rate':0.0,
+        'assistant_rate':0.0,
+        'overtime_multiplier':1.0
+        }
+    def _check_rates(self, cr, uid, ids, context={}):
+        for rates in self.browse(cr, uid, ids, context=context):
+            if (rates.technical_rate<1.0 or rates.assistant_rate<1.0):
+                return False
+        return True
+    
+    def _check_multipliers(self, cr, uid, ids, context={}):
+        for multipliers in self.browse(cr, uid, ids, context=context):
+            if (multipliers.overtime_multiplier<1.0 or multipliers.holiday_multiplier<1.0):
+                return False
+        return True
+    _constraints = [
+        (_check_rates,'Rates must be greater or equal to one',['technical_rate','assistant_rate']
+         ),
+        (_check_multipliers,'Multipliers must be greater or equal to one',['overtime_multiplier','holiday_multiplier']
+         ) 
+                ]
+    _sql_constraints = [
+        ('contract_line_unique',
+        'UNIQUE(categ_id,product_id,contract_pricelist_id)',
+        'Contract only allow a line to a single category or product')     
+    ]
+
+class HolidayCalendar(orm.Model):
+    _name = 'holiday.calendar'
+
+    _columns = {
+        'name':fields.char(size=256,string="Name"),
+        'date':fields.date(required=True,string="Date"),
+        'notes':fields.text(string="Notes")
+        }
+                        
+                 
+
+
+
+                        
