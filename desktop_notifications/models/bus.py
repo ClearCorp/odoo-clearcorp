f5f61b575da47ccc6d246862d8587ea012df34f7
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-03-01 17:06:50 -0600

    [REF]-desktop_notifications: Resources

--- desktop_notifications/models/bus.py
+++ desktop_notifications/models/bus.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © <2016> ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import models
+from openerp import models, api
 
 
 class ImBus(models.Model):
@@ -9,5 +9,29 @@ class ImBus(models.Model):
     _inherit = 'bus.bus'
 
     def sendmany(self, cr, uid, notifications):
-        print "\n Metodo heredado \n"
+        print "\n Metodo heredado \n", notifications
+        notification =\
+            [[(u'cc_notificaciones', 'im_chat.session', 5),
+              {
+                  'create_date': '2016-03-01 19:18:55',
+                  'to_id': (2, u'59f32ef1-1557-464d-a73f-20d2b85d9f1f'),
+                  'message': '1,2,3,4,5,6,7,8,9',
+                  'type': u'message',
+                  'id': 155,
+                  'from_id': (5, u'lesmed')
+               }
+              ]] 
         super(ImBus, self).sendmany(cr, uid, notifications)
+
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model
+    @api.returns('self', lambda value: value.id)
+    def create(self, vals):
+        print "\n create heredado \n"
+        bus_obj = self.env['bus.bus']
+        bus_obj.sendone("59f32ef1-1557-464d-a73f-20d2b85d9f1f",
+            "Mensaje de prueba")
+        return models.Model.create(self, vals)
--- desktop_notifications/static/src/js/notification.js
+++ desktop_notifications/static/src/js/notification.js
@@ -1,3 +1,9 @@
 /* © <YEAR(S)> ClearCorp
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
+(function(){
+    
+    
+    console.log('Nuevo javascript')
+    
+}
