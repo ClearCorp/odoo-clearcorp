94c81c044d83bc643ee34dd96e524c760a6ab2a6
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-03-01 12:07:00 -0600

    [REF]-desktop_notifications: folder rename

--- desktop_notifications/models/bus.py
+++ desktop_notifications/models/bus.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # © <2016> ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import models, api
+from openerp import models
 
 
 class ImBus(models.Model):
 
     _inherit = 'bus.bus'
 
-    @api.model
-    def sendmany(self, notifications):
+    def sendmany(self, cr, uid, notifications):
         print "\n Metodo heredado \n"
-        super(ImBus, self).sendmany(self._cr, self._uid, notifications)
+        super(ImBus, self).sendmany(cr, uid, notifications)
