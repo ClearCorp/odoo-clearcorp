4a8764a35601c3b65090313e8f7905d23017ccf1
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-03-03 10:03:37 -0600

    [FIX]-desktoo_notification: replace content by mistake

--- mail_private_message/models/notification.py
+++ mail_private_message/models/notification.py
@@ -19,13 +19,30 @@
 #
 ##############################################################################
 
-from openerp import models, fields
+from openerp import models, api
 
 
-class DesktopSession(models.Model):
-    _name = 'desktop.session'
-    _order = 'id desc'
-    _rec_name = 'uuid'
+class Notification(models.Model):
 
-    uuid = fields.Char(string='UUID', size=50, select=True)
-    message_ids = fields.One2many('desktop.message', )
+    _inherit = 'mail.notification'
+
+    @api.model
+    def _notify(self, message_id, partners_to_notify=None,
+                force_send=False, user_signature=True):
+        if 'notify' in self.env.context and not self.env.context['notify']:
+            partner_obj = self.env['res.partner']
+            message = self.env['mail.message'].browse(message_id)
+            partners_to_notify = self.env.context.get('partners_to_notify', [])
+            if message.privacity == 'private':
+                user_obj = self.pool.get('res.users')
+                clean_partners = []
+                # clean partners to notify only internal users
+                for partner in partner_obj.browse(partners_to_notify):
+                    for user in partner.user_ids:
+                        if user_obj.has_group(
+                                self._cr, user.id, 'base.group_user'):
+                            clean_partners.append(partner.id)
+                partners_to_notify = clean_partners
+        return super(Notification, self)._notify(
+            message_id, partners_to_notify=partners_to_notify,
+            force_send=force_send, user_signature=user_signature)
