e43dbea57c73b4405a5564ce3f640192d2f4f9f3
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-03-18 17:02:47 -0600

    [FIX]-password_security: secure cookie

--- passwork_security/controllers/web.py
+++ passwork_security/controllers/web.py
@@ -4,16 +4,38 @@
 from openerp import http
 from openerp.addons.web.controllers import main
 from openerp.http import request
+from werkzeug.utils import cached_property
+from werkzeug.wrappers import BaseRequest
+from werkzeug.contrib.securecookie import SecureCookie
+import openerp
+import os
+
+SECRET_KEY = "23dfsDSghaSAgJUUYjgbnMiopgs"
 
 
 class Home(main.Home):
 
+    def _load_cookie(self, name):
+        _data = request.httprequest.cookies.get('session_data')
+        return SecureCookie.load_cookie(request.httprequest, key=name
+                                        , secret_key=SECRET_KEY)
+
     @http.route('/web/login', type='http', auth="none")
     def web_login(self, redirect=None, **kw):
         print "\nlogin custom\n"
-        request_obj = request
-        res = super(Home, self).web_login(redirect, **kw)
-        if res.qcontext:
-            print "qcontext: ",  res.qcontext, request_obj
-        print res
-        return res
+        session = request.session
+        response = super(Home, self).web_login(redirect, **kw)
+        secure_cookie = self._load_cookie('session_data')
+        login_attemps = int(secure_cookie['login_attemps'])
+        secure_cookie['login_attemps'] = str(login_attemps + 1)
+        if secure_cookie.should_save:
+            print secure_cookie
+        else:
+            secure_cookie['login_attemps'] = str(0)
+            print secure_cookie
+            #ession_data['login_attemps'] = 0
+        if hasattr(response, 'set_cookie'):
+            secure_cookie.save_cookie(response, 'session_data', httponly=True)
+        print "qcontext: ", response.qcontext, session
+        print response
+        return response
