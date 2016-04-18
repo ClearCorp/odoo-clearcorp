8b7ac24f7c11790a2d0586225435e0d96b25c3d6
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-04-18 14:09:22 -0600

    [ADD]-hr_payroll_account_line: Module added to migration v9

--- hr_payroll_account_line_partner/models/hr_payroll_account_line_partner.py
+++ hr_payroll_account_line_partner/models/hr_payroll_account_line_partner.py
@@ -1,49 +1,29 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import osv, fields
+from openerp import models, fields
 
-class SalaryRule(osv.Model):
+
+class SalaryRule(models.Model):
 
     _RULE_TYPES = [
-                   ('partner', 'Partner'),
-                   ('employee', 'Employee'),
-                   ]
+        ('partner', 'Partner'),
+        ('employee', 'Employee'),
+    ]
 
     _inherit = 'hr.salary.rule'
+    rule_type_credit = fields.Selection(
+        _RULE_TYPES, string='Credit Rule Type', default='employee'),
+    rule_type_debit = fields.Selection(
+        _RULE_TYPES, string='Debit Rule Type', default='employee'),
+    res_partner_credit = fields.Many2one(
+        'res.partner', string='Credit Partner'),
+    res_partner_debit = fields.Many2one(
+        'res.partner', string='Debit Partner'),
 
-    _columns = {
-                'rule_type_credit': fields.selection(_RULE_TYPES, string='Credit Rule Type'),
-                'rule_type_debit': fields.selection(_RULE_TYPES, string='Debit Rule Type'),
-                'res_partner_credit': fields.many2one('res.partner', string='Credit Partner'),
-                'res_partner_debit': fields.many2one('res.partner', string='Debit Partner'),
-                }
 
-    _defaults = {
-                 'rule_type_credit': 'employee',
-                 'rule_type_debit': 'employee',
-                 }
-
-class PaySlip(osv.Model):
+class PaySlip(models.Model):
 
     _inherit = 'hr.payslip'
 
