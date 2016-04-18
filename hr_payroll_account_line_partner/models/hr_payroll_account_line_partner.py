eb1709bc9d2edd42819505ff4624f4c8241f5d45
Lesmed Gutierrez <lesmed.gutierrez@clearcorp.co.cr>
2016-04-18 15:48:38 -0600

    [FIX]-hr_payroll_account_line: Fix pep8 issues

--- hr_payroll_account_line_partner/models/hr_payroll_account_line_partner.py
+++ hr_payroll_account_line_partner/models/hr_payroll_account_line_partner.py
@@ -2,7 +2,7 @@
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class SalaryRule(models.Model):
@@ -27,51 +27,77 @@ class PaySlip(models.Model):
 
     _inherit = 'hr.payslip'
 
-    def process_sheet(self, cr, uid, ids, context=None):
-        res = super(PaySlip, self).process_sheet(cr, uid, ids, context=context)
-        for payslip in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def process_sheet(self):
+        res = super(PaySlip, self).process_sheet()
+        for payslip in self:
             for line in payslip.line_ids:
                 if line.salary_rule_id:
                     # Check is salary rule has debit
                     if line.salary_rule_id.account_debit:
                         # Check if rule has rule_type_debit
                         if line.salary_rule_id.rule_type_debit:
-                            move_line_obj = self.pool.get('account.move.line')
+                            move_line = self.env['account.move.line']
                             # Check the rule_type if partner
-                            if line.salary_rule_id.rule_type_debit == 'partner':
-                                move_line_ids = move_line_obj.search(cr, uid,
-                                [('move_id','=',payslip.move_id.id), ('debit','=',line.total),
-                                 ('account_id','=',line.salary_rule_id.account_debit.id)], context=context)
-                                if move_line_ids:
-                                    move_line_obj.write(cr, uid, move_line_ids[0],
-                                    {'partner_id': line.salary_rule_id.res_partner_debit.id}, context=context)
+                            if line.salary_rule_id.rule_type_debit ==\
+                                    'partner':
+                                move_lines = move_line.search(
+                                    [('move_id', '=', payslip.move_id.id),
+                                     ('debit', '=', line.total),
+                                     ('account_id', '=',
+                                      line.salary_rule_id.account_debit.id)])
+                                if move_lines:
+                                    _rule = line.salary_rule_id
+                                    _partner = _rule.res_partner_debit.id
+                                    move_lines.write(
+                                        {
+                                            'partner_id': _partner
+                                        })
                             # Rule type is employee
                             else:
-                                move_line_ids = move_line_obj.search(cr, uid,
-                                [('move_id','=',payslip.move_id.id), ('debit','=',line.total),
-                                 ('account_id','=',line.salary_rule_id.account_debit.id)], context=context)
-                                if move_line_ids:
-                                    move_line_obj.write(cr, uid, move_line_ids[0],
-                                    {'partner_id': payslip.employee_id.address_home_id.id}, context=context)
+                                move_lines = move_line.search(
+                                    [('move_id', '=', payslip.move_id.id),
+                                     ('debit', '=', line.total),
+                                     ('account_id', '=',
+                                      line.salary_rule_id.account_debit.id)])
+                                if move_lines:
+                                    _employee = payslip.employee_id
+                                    _id = _employee.address_home_id.id
+                                    move_lines.write(
+                                        {
+                                            'partner_id': _id
+                                        })
                     # Credit check if salary rule has credit
                     if line.salary_rule_id.account_credit:
                         # Check if rule has rule_type credit
                         if line.salary_rule_id.rule_type_credit:
-                            move_line_obj = self.pool.get('account.move.line')
+                            move_line = self.env['account.move.line']
                             # Check if rule_type is partner
-                            if line.salary_rule_id.rule_type_credit == 'partner':
-                                move_line_ids = move_line_obj.search(cr, uid,
-                                    [('move_id','=',payslip.move_id.id), ('credit','=',line.total),
-                                     ('account_id','=',line.salary_rule_id.account_credit.id)], context=context)
-                                if move_line_ids:
-                                    move_line_obj.write(cr, uid, move_line_ids[0],
-                                    {'partner_id': line.salary_rule_id.res_partner_credit.id}, context=context)
+                            if line.salary_rule_id.rule_type_credit ==\
+                                    'partner':
+                                move_lines = move_line.search(
+                                    [('move_id', '=', payslip.move_id.id),
+                                     ('credit', '=', line.total),
+                                     ('account_id', '=',
+                                      line.salary_rule_id.account_credit.id)],)
+                                if move_lines:
+                                    _rule = line.salary_rule_id
+                                    _partner = _rule.res_partner_credit.id
+                                    move_lines.write(
+                                        {
+                                            'partner_id': _partner})
                             # Rule type is employee
                             else:
-                                move_line_ids = move_line_obj.search(cr, uid,
-                                    [('move_id','=',payslip.move_id.id), ('credit','=',line.total),
-                                     ('account_id','=',line.salary_rule_id.account_credit.id)], context=context)
-                                if move_line_ids:
-                                    move_line_obj.write(cr, uid, move_line_ids[0],
-                                    {'partner_id': payslip.employee_id.address_home_id.id}, context=context)
+                                move_lines = move_line.search(
+                                    [('move_id', '=', payslip.move_id.id),
+                                     ('credit', '=', line.total),
+                                     ('account_id', '=',
+                                      line.salary_rule_id.account_credit.id)])
+                                if move_lines:
+                                    _employee = payslip.employee_id
+                                    _id = _employee.address_home_id.id
+                                    move_lines.write(
+                                        {
+                                            'partner_id': _id
+                                        })
         return res
