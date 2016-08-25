f20e6ba104918626b52b272e055a057bc81eb437
Antonio Hidalgo <antonio.hidalgo@clearcorp.co.cr>
2016-08-25 10:46:41 -0600

    Migrated project_issue_priority and project_issue_stage

--- project_issue_stage/README.rst
+++ project_issue_stage/README.rst
@@ -2,13 +2,12 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-======================
-Project Issue Priority
-======================
+===================
+Project Issue Stage
+===================
 
-This module was written to extend the functionality of project_issue
-in order to support issue priority. The priority representation is based on
-a 5-star rating.
+This module was written to add an issue stage type, to further classify each
+task state.
 
 Credits
 =======
--- project_issue_stage/models/project_issue_stage.py
+++ project_issue_stage/models/project_issue_stage.py
@@ -3,12 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api, _
-from openerp.exceptions import Warning
-from datetime import date
 
 
-class project_issue_type(models.Model):
+class ProjectIssueType(models.Model):
     _inherit = 'project.task.type'
     
     type = fields.Selection([('task', 'Task'), ('issue', 'Issue'),
-                             ('both', 'Both')], 'Stage type', required=True)
+                             ('both', 'Both')], 'Stage Type', required=True)
