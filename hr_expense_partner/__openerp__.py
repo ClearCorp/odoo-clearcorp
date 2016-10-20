b169f59bc425ed5d10d34114e6ceb7053b3d283d
David Ocampo <david.ocampo@clearcorp.co.cr>
2016-10-20 16:54:03 -0600

    [FIX] - Hr Expense Partner: Fixed module
    Modified: Moved the module to the TODO-9.0 file to the branch root, make initial commit, rename to hr_expense_partner, maked commit, added README, modified licenses, added icone, modified expense_line_partner.py to hr_expense.py, deleted hr_expense_line class, added the required fields, added new view, deleted old views, added new method _prepare_move_line with herency hr.expense and added conditions

--- hr_expense_partner/__openerp__.py
+++ hr_expense_partner/__openerp__.py
@@ -1,46 +1,27 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 ClearCorp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Expense line partner',
+    'name': 'Hr Expense Partner',
     'version': '1.0',
-    'author' : 'ClearCorp',
+    'author': 'ClearCorp',
     'complexity': 'normal',
     'website': 'http://clearcorp.co.cr',
     'category': 'Human Resources',
-    'description': """
-Extends hr_expense
-===============================================
-Adds partner field to choose supplier for a given expense line, At the account move line creation, sets 
-the chosen partner for that given line instead of the hr_expense supplier
-""",
-    'depends': ['hr_expense'],
+    'description': ' Adds partner field to choose supplier for a given expense'
+                   ' line, At the account move line creation, sets '
+                   ' the chosen partner for that given line instead of '
+                   ' the hr_expense supplier',
+    'depends': [
+        'hr_expense'
+    ],
     'init_xml': [],
     'demo_xml': [],
     'data': [
-             'expense_line_partner_view.xml'],
+        'views/hr_expense_view.xml'
+    ],
     'license': 'AGPL-3',
     'installable': True,
     'active': False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
